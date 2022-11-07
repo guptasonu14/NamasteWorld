4f5497754e2267f372c45788d7c69b105fdbb99d
guptasonu14 <sguptabudha@gmail.com>
2022-11-07 17:57:23 +0530

    first commit

---
+++ src/main/java/Namaste/NamasteWorldApplication.java
@@ -0,0 +1,10 @@
+package Namaste;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamasteWorldApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NamasteWorldApplication.class,args);
+    }
+}
