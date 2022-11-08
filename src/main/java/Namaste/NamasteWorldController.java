3a45b5539f651da7433eb45e295d2dd3516149c4
guptasonu14 <sguptabudha@gmail.com>
2022-11-08 15:19:44 +0530

    add html page

--- src/main/java/Namaste/NamasteWorldController.java
+++ src/main/java/Namaste/NamasteWorldController.java
@@ -2,15 +2,15 @@ package Namaste;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class NamasteWorldController {
 
     @RequestMapping("/hellospring")
-    @ResponseBody
+
     public String helloSpring(){
-        return "Hello Spring";
+        return "index";
     }
 
 }
